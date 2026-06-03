1fcd41b3e89f597044ad1a47c23ef42fb36a432f
AjaySaragadam <saragadamajay83@gmail.com>
2026-06-03 16:52:42 +0530

    full basic url

--- tests/UI/Practice.spec.ts
+++ tests/UI/Practice.spec.ts
@@ -5,10 +5,11 @@ import Utils  from '../../Utilities/Utils';
 test('Playwright Automation Prcatice Test', {tag : ['@basictest']}, async ({page}) =>{
     const utils = new Utils(page);
 
-    await page.goto('/', {
-waitUntil: 'domcontentloaded',
+   await page.goto('https://demo.automationtesting.in/', {
+  waitUntil: 'load',
   timeout: 60000
 });
+``
     await utils.clickOnWebElement('#btn2');
     await page.locator('#imagesrc').setInputFiles('./test-data/Payslip India January_2026.pdf');
 });
