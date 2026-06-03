72e95a018d70e0e05a376f3db285825ba5d69a4a
AjaySaragadam <saragadamajay83@gmail.com>
2026-06-03 16:27:14 +0530

    updated base url in config file

--- playwright.config.js
+++ playwright.config.js
@@ -5,7 +5,7 @@ export default defineConfig ({
     testDir : './tests',
     use: {
             headless : process.env.CI? true : false,
-            baseURL : 'https://demo.automationtesting.in/Index.html',
+            baseURL : 'https://demo.automationtesting.in/',
             viewport: null,
             launchOptions :{
               args : ['--start-maximized']
